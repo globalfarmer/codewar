405a728fd5eb6626d653bc879b1ed1e344da0967
DoVietAnh <anhdv@projectkit.net>
2017-04-13 18:50:09 +0700

    finish bot_version1

--- system.cpp
+++ system.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 const int dx[9] = {-1, -1, -1,  0, 0,  1, 1, 1, 0};
 const int dy[9] = {-1,  0,  1, -1, 1, -1, 0, 1, 0};
-const string DIRECT_NAME[4] = { "LEFT", "UP", "RIGHT", "DOWN"};
-const int dir_x[4] = { 0, -1, 0, 1};
-const int dir_y[4] = {-1,  0, 1, 0};
+const string DIRECT_NAME[4] = { "RIGHT", "DOWN", "LEFT", "UP"};
+const int dir_x[4] = { 0,  1,  0, -1};
+const int dir_y[4] = { 1,  0, -1,  0};
 const string EMPTY_LINE = "000000000000000000000000000000";
 const int oo = 1e6;;
 const int INF = 1e9;
@@ -43,9 +43,10 @@ public:
 	int getScore(const int&, const State&);
 	State removePlayer(int playerId, State lastState ); 
 	State updateGame(int, const vector<State>& states, State, const string &decision);
-	int tryingFill(const int&, State&, const vector<vector<int> >&, const vector<vector<int> >&, const int&);
-	bool findPath(const pair<int, int>&, vector<string>&, int, State&, const vector<vector<int> >&, const vector<vector<int> >& g, const int&);
-	int evaluateScore(int playerId, State state);
+	bool findACertainPath_inBound(const pair<int, int>&, int, int ,int, int);
+	// playerId, state, mark, ret, steps, _d, _label, x, y, r, c;
+	void findACertainPath_goAround(int,	const State&,const vector<vector<bool> >&, int&, queue<string>&,
+								const vector<pair<int, int> >&,const vector<string>&, int, int, int, int);
 } helper;
 
 class Player
@@ -55,6 +56,8 @@ public:
 	int numberOfPlayer;
 	int ownId;
 	int turn;
+	string lastStep;
+	queue<string> steps;
 //	Player(const Player&);
 	/**
 	* @param int _numberOfPlayer
@@ -65,7 +68,7 @@ public:
 	Player(int, int, const vector<string>&, const vector<pair<int, int> >&);
 	~Player();
 	string getDecision();
-	bool Player::findACertainPath(int playerId, State state, queue<pair<int, int> > &steps);
+	bool findACertainPath(int playerId, State state, queue<string> &steps);
 	void updateState(const State&);
 	int getScore(int);
 };	
@@ -96,14 +99,14 @@ Player::~Player()
 {
 
 }
-bool Player::findACertainPath(int playerId, State state, queue<pair<int, int> > &steps)
+bool Player::findACertainPath(int playerId, State state, queue<string> &steps)
 {
 	if( state.playersPos[playerId-1] == make_pair(-1, -1) )
 		return false;
 	queue<pair<int, int> > q;
-	int maxL = 0;
+	int maxL = -1;
 	// find d[i][j] is minimum turn in Which an enemy can reach to [i,j]
-	vector<vector<int> > d = vector<vector<int> >(20, vector<int>(30, -1));
+	vector<vector<int> > d = vector<vector<int> >(20, vector<int>(30, oo));
 	q = queue<pair<int, int> >();
 	for(int i = 0; i < state.playersPos.size(); i++) 
 		if( i+1 != playerId && state.playersPos[i] != make_pair(-1, -1))
@@ -118,7 +121,7 @@ bool Player::findACertainPath(int playerId, State state, queue<pair<int, int> > 
 		for(int j = 0; j < 4; j++) 
 		{
 			pair<int, int> newPos = make_pair(pos.first + dir_x[j], pos.second + dir_y[j]);
-			if( !helper.isOuter(newPos) && d[newPos.first][newPos.second] == -1) 
+			if( !helper.isOuter(newPos) && d[newPos.first][newPos.second] == oo) 
 			{
 				d[newPos.first][newPos.second] = d[pos.first][pos.second] + 1;
 				maxL = max(maxL, d[newPos.first][newPos.second]);
@@ -126,99 +129,127 @@ bool Player::findACertainPath(int playerId, State state, queue<pair<int, int> > 
 			}
 		}
 	}
-	vector<vector<int> > mark;
+	if(maxL == -1)maxL = 30;
+	// for(int i = 0; i < 20; i++) {
+	// 	for(int j = 0; j < 30; j++) {
+	// 		cout << d[i][j] <<" ";
+	// 	}
+	// 	cout << endl;
+	// }
+	vector<vector<bool> > mark;
 	string llabel[4] = {"RIGHT", "DOWN", "LEFT", "UP"};
 	// "RIGHT", "DOWN", "LEFT", "UP";	
 	vector<string> label_cw(llabel, llabel+4);
-	pair<int> dd[4] = {make_pair(0,1), make_pair(1, 0), make_pair(0,-1), make_pair(-1, 0)};
+	pair<int, int> dd[4] = {make_pair(0,1), make_pair(1, 0), make_pair(0,-1), make_pair(-1, 0)};
 	vector<pair<int, int> > d_cw(dd, dd+4);;
 	// "LEFT", "DOWN", "RIGHT", "UP"
 	vector<string> label_ccw = label_cw;
 	swap(label_ccw[0], label_ccw[2]);
 	vector<pair<int, int> > d_ccw = d_cw;;
-	swap(d_ccw[0], label_ccw[2]);
+	swap(d_ccw[0], d_ccw[2]);
 	int ret = -1;
 	for(int l = maxL; l >= 4; l--) 
-		if( d[state.playersPos[playerId-1].fist][state.playersPos[playerId-1].second] >= l )
+		if( d[state.playersPos[playerId-1].first][state.playersPos[playerId-1].second] >= l )
 	{
 		mark = vector<vector<bool> >(20, vector<bool>(30, false));
-		for(int i = 0; i < 20; i++) {
+		for(int i = 0; i < 20; i++) 
+		{
 			for(int j = 0; j < 30; j++) mark[i][j] = d[i][j] >= l;
 		}
-		for(int r = 2; r <= 20; r++) 
+		// if( l == maxL)
+		// for(int i = 0; i < 20; i++) 
+		// {
+		// 	for(int j = 0; j < 30; j++) cout << mark[i][j]; cout << endl;
+		// }
+		for(int r = 2; r <= min(l, 20); r++) 
 		{
-			for(int c = 2; c <= 30; c++) 
+			for(int c = 2; c <= min(l, 30)-r; c++) 
 			{
 				for(int x = 0; x <= 20 - r; x++) 
 				{
 					for(int y = 0; y <= 30 - c; y++)
 					{
-						if( helper.inBound(state.playersPos[playerId-1], x, y, r, c) ) 
+						if( helper.findACertainPath_inBound(state.playersPos[playerId-1], x, y, r, c) ) 
 						{
+							// cout <<state.playersPos[playerId-1].first<<" "<<state.playersPos[playerId-1].second<<" "<<x <<" "<<y<<" "<<r<<" "<<c<<endl;
+							int tmp = -1;
+							queue<string> tmpStep;
 							helper.findACertainPath_goAround(
 								playerId, 
 								state, 
 								mark,
-								ret,
-								steps,
+								tmp,
+								tmpStep,
 								d_cw,
-								label_cw);
-							helper.findACertainPath_goAround(
-								playerId, 
-								state, 
-								mark,
-								ret,
-								steps,
-								d_ccw,
-								label_ccw);
+								label_cw,
+								x,
+								y,
+								r,
+								c);
+							if( tmp > ret && tmpStep.size() <= l && !
+								(
+									(this->lastStep=="DOWN" && tmpStep.front()=="UP") ||
+									(this->lastStep=="UP" && tmpStep.front()=="DOWN") ||
+									(this->lastStep=="RIGHT" && tmpStep.front()=="LEFT") ||
+									(this->lastStep=="LEFT" && tmpStep.front()=="RIGHT")
+								))
+							{
+								ret = tmp;
+								steps = tmpStep;
+							}
 						}
 					}
 				}
 			}
 		}
 	}
+	cout << "RETURN "<<(ret > 0)<<endl;
 	return ret > 0;
 }
 bool Helper::findACertainPath_inBound(const pair<int, int> &pos, int x, int y, int r, int c)
 {
-	bool ret = (x == pos.first && pos.second >= y && pos.second < y + c);
-	ret |= (x + r - 1 == pos.first && pos.second >= y && pos.second < y + c)
-	ret |= (y == pos.second && pos.first >= x && pos.first < x + r);
-	ret |= (y + c - 1== pos.second && pos.first >= x && pos.first < x + r);
-	return ret;
+	return (x == pos.first && pos.second >= y && pos.second < y + c) ||
+	       (x + r - 1 == pos.first && pos.second >= y && pos.second < y + c) ||
+	       (y == pos.second && pos.first >= x && pos.first < x + r) ||
+	       (y + c - 1 == pos.second && pos.first >= x && pos.first < x + r);
 }
 void Helper::findACertainPath_goAround(int playerId,
 									const State& _state, 
 									const vector<vector<bool> >& mark, 
 									int &ret, 
-									queue<pair<int, int> >& steps,
-									const vector<pair<int, int> >& _d
-									const vector<string>& _label) 
+									queue<string >& steps,
+									const vector<pair<int, int> >& _d,
+									const vector<string>& _label,
+									int x, int y, int r, int c) 
 {
-	State tempState = _state;
-	int idx
-	queue<pair<int, int> > tmpStep;
+	State tmpState = _state;
+	int idx;
+	queue<string> tmpStep;
 	int cnt = 0;
+	pair<int, int> newPos, pos = _state.playersPos[playerId-1];
 	for(int i = 0; i < 4; i++) 
 	{
-		pair<int, int> newPos, pos = _state.playersPos[playerId-1];
+		if(!((i==0 && pos.first==x) || (i==1 && pos.second==y) || (i==2 && pos.first==x+r-1) || (i==3 && pos.second==y+c-1)))
+			continue;
 		newPos.first = pos.first + _d[i].first;
-		newPos.seoncd = pos.second + _d[i].second;
-		if( helper.inBound(newPos, x, y, r, c) && mark[newPos.first][newPos.second])
+		newPos.second = pos.second + _d[i].second;
+		if( helper.findACertainPath_inBound(newPos, x, y, r, c) && mark[newPos.first][newPos.second])
 		{
+			// cout << " first direction " << i << endl;
 			idx = i;
 			for(int loop = 0; loop < 4; loop++) 
 			{
-				while(cnt < 2 && inBound(newPos, x, y, r, c)) {
+				while(cnt < 2 && helper.findACertainPath_inBound(newPos, x, y, r, c)) {
+					// cout << " --- " << newPos.first <<" "<<newPos.second << endl;
 					if( mark[newPos.first][newPos.second] == false ) return;
-					tmpStep.push(_d[i]);
+					tmpStep.push(_label[idx]);
 					if( tmpState.table[newPos.first][newPos.second] != tmpState.table[pos.first][pos.second] && 
 						(tmpState.table[newPos.first][newPos.second] == playerId * 2 - 1 + '0' || 
 						 tmpState.table[pos.first][pos.second] == playerId * 2 - 1 + '0')
 						)
 						cnt++;
 					if( tmpState.table[newPos.first][newPos.second] != playerId * 2 - 1 + '0' )
-						tmpState.table[newPos.first][newPos.second] == playerId * 2 + '0';
+						tmpState.table[newPos.first][newPos.second] = playerId * 2 + '0';
 					if( cnt == 2 ) break;
 					pos = newPos;
 					newPos.first = pos.first + _d[idx].first;
@@ -227,8 +258,10 @@ void Helper::findACertainPath_goAround(int playerId,
 				idx = (idx + 1) % 4;
 				newPos.first = pos.first + _d[idx].first;
 				newPos.second = pos.second + _d[idx].second;
-				break;
 			}
+			// cout << "Find a cetern path" << endl;
+			// cout << "cnt "<< cnt << endl;
+			// tmpState.printState();
 			if(cnt == 2)
 			{
 				int tmp = helper.stablize(playerId, tmpState);
@@ -242,241 +275,19 @@ void Helper::findACertainPath_goAround(int playerId,
 		}
 	}
 }
-// string Player::minimax2(int depth, vector<State> states, vector<string> decisions) 
-// {
-// 	if( depth == 11 ) 
-// 	{
-// 		return (this->evaluateScore(this->ownId, states) + helper.getScore(this->ownId, states) )
-// 		      -(this->evaluateScore(this->ownId % 2 + 1, states) + helper.getScore(this->ownId%2+1, states));
-// 	}
-// 	if( depth > 1 && depth % 2 == 1) 
-// 	{
-
-// 	}
-// }
-pair<int, string> Player::getDecision2(int depth, int playerId, State prevState, State curState) 
-{
-	// cout << "depth "<<depth << endl;
-	if( depth == 3) 
-	{
-		return make_pair(helper.getScore(playerId, curState) + helper.evaluateScore(playerId, curState), "");
-	}
-	if( curState.playersPos[playerId-1] == make_pair(-1, -1) )
-		return make_pair(-INF, "");
-	int optValue = (depth % 2 ? -INF : INF);
-	string decision;
-	vector<State> states;
-	if( depth > 1)
-	{
-		states.push_back(prevState);
-		states.push_back(curState);
-	}
-	else
-	{
-		states.push_back(curState);
-	}
-	// cout << "depth "<<depth << endl;
-
-	for(int j = 0; j < 4; j++) 
-	{
-		// cout << j << endl;
-		State newState = helper.updateGame(playerId, states, curState, DIRECT_NAME[j]);
-		// for(int ii = 0; ii < newState.playersPos.size(); ii++)
-			// cout << newState.playersPos[ii].first <<" "<<newState.playersPos[ii].second << endl;
-		pair<int, string> tmp = this->getDecision2(depth+1, playerId % 2 + 1, curState, newState);
-		if( depth % 2 && tmp.first > optValue)
-		{
-			optValue = tmp.first;
-			decision = DIRECT_NAME[j];
-		}
-		else if( depth % 2 == 0 && tmp.first < optValue)
-		{
-			optValue = tmp.first;
-			decision = DIRECT_NAME[j];
-		}
-	}
-	return make_pair(optValue, decision);
-}
-pair<int, string> Player::getDecision3()
-{
-	return make_pair(0, "RIGHT");
-}
-pair<int, string> Player::getDecision4() 
-{
-	return make_pair(0, "RIGHT");
-}
 string Player::getDecision()
 {
-	pair<int, string> ret;
-	if( this->numberOfPlayer == 2) {
-		cout << "Game for two"<<endl;
-		ret = this->getDecision2(1, this->ownId, State(), this->states[this->states.size()-1]);
-	}
-	else if( this->numberOfPlayer == 3)
-		ret = this->getDecision3();
-	else if( this->numberOfPlayer == 4)
-		ret = this->getDecision4();
-	return ret.second;
-	// State lastState = this->states[this->states.size()-1];
-	// int maxStat = 1;
-	// for(int i = 0; i < this->numberOfPlayer; i++) maxStat *= 4;
-	// if( lastState.table[lastState.playersPos[this->ownId-1].first][lastState.playersPos[this->ownId-1].second] - '0' == this->ownId*2-1) {
-	// 	vector<string> decisions = vector<string>(this->numberOfPlayer, "");
-	// 	for(int stat = 0; stat <= maxStat; stat++) {
-	// 		for(int j = 0, tmp=stat; j < this->numberOfPlayer; j++) 
-	// 		{
-	// 			decision[j] = DIRECT_NAME[tmp%4];
-	// 			tmp/=4;
-	// 		}
-	// 	}
-	// 	return decision;
-	// }
-	return DIRECT_NAME[this->ownId-1];
-	// return DIRECT_NAME[rand() % 4];//"RIGHT";
-}
-int Helper::evaluateScore(int playerId, State state) 
-{
-	if( state.playersPos[playerId-1] == make_pair(-1, -1) )
-		return -oo;
-	vector<pair<int, int> > myUnstable, myStable;
-	for(int i = 0; i < 20; i++) 
+	if(this->steps.empty())
 	{
-		for(int j = 0; j < 30; j++) 
-			if( state.table[i][j] != '0' )
-			{
-				if( (state.table[i][j] - '0' + 1) / 2 != playerId )
-				{
-					if( (state.table[i][j] - '0') % 2 ) 
-						state.table[i][j] = 's';
-					else
-						state.table[i][j] = 'u';
-				}
-				else 
-				{
-					if( ( (state.table[i][j] - '0') % 2 == 0) )
-						myUnstable.push_back(make_pair(i, j));
-					else
-						myStable.push_back(make_pair(i, j));
-				}
-				
-			}
-
+		bool haveAPath = this->findACertainPath(this->ownId, this->states[this->states.size()-1], this->steps);
+		if( !haveAPath){
+			this->steps = queue<string>();
+			for(int i = 0; i < 4; i++) 
+				this->steps.push(DIRECT_NAME[i]);
+		}
 	}
-	// cout <<" OK "<<endl;
-	// for(int i = 0; i < 20; i++) cout << state.table[i]<<endl;
-	if( myUnstable.size() > 0) 
-	{
-		queue<pair<int, int> > q;
-		// find d[i][j] is minimum turn in Which an enemy can reach to [i,j]
-		vector<vector<int> > d = vector<vector<int> >(20, vector<int>(30, oo));
-		q = queue<pair<int, int> >();
-		for(int i = 0; i < state.playersPos.size(); i++) 
-			if( i+1 != playerId && state.playersPos[i] != make_pair(-1, -1))
-		{
-			d[state.playersPos[i].first][state.playersPos[i].second] = 0;
-			q.push(state.playersPos[i]);
-		}
-		while(!q.empty()) 
-		{
-			pair<int, int> pos = q.front(); q.pop();
-			// cout << pos.first <<" "<<pos.second << endl;
-			for(int j = 0; j < 4; j++) 
-			{
-				pair<int, int> newPos = make_pair(pos.first + dir_x[j], pos.second + dir_y[j]);
-				if( !helper.isOuter(newPos) && d[newPos.first][newPos.second] == oo) 
-				{
-					d[newPos.first][newPos.second] = d[pos.first][pos.second] + 1;
-					q.push(newPos);
-				}
-			}
-		}
-		cout <<" OK "<<endl;
-		// for(int i = 0; i < 20; i++) {
-		// 	for(int j = 0; j < 30; j++) {
-		// 		cout << d[i][j] <<" ";
-		// 	}
-		// 	cout << endl;
-		// }
-		// find g[i][j] is minimum turn in which I can reach to a my stable ( but my stable still be changed in game ????)
-		vector<vector<int> > g = vector<vector<int> >(20, vector<int>(30, oo));
-		q = queue<pair<int, int> >();
-		for(int i = 0; i < myStable.size(); i++) 
-		{
-			g[myStable[i].first][myStable[i].second] = 0;
-			q.push(myStable[i]);
-		}
-		while(!q.empty())
-		{
-			pair<int, int> pos = q.front(); q.pop();
-			for(int j = 0; j < 4; j++)
-			{
-				pair<int, int> newPos = make_pair(pos.first + dir_x[j], pos.second + dir_y[j]);
-				if( !helper.isOuter(newPos) && 
-					g[newPos.first][newPos.second] == oo  &&
-					state.table[newPos.first][newPos.second] - '0' != playerId * 2)
-				{
-					g[newPos.first][newPos.second] = g[pos.first][pos.second] + 1;
-					q.push(newPos);
-				}
-			}
-		}
-		// for(int i = 0; i < 20; i++) {
-		// 	for(int j = 0; j < 30; j++) {
-		// 		cout << g[i][j] <<" ";
-		// 	}
-		// 	cout << endl;
-		// }
-		int minD = oo;
-		for(int i = 0; i < myUnstable.size(); i++) {
-			minD = min(minD, d[myUnstable[i].first][myUnstable[i].second]);
-			// cout << myUnstable[i].first <<" "<<myUnstable[i].second <<" "<<d[myUnstable[i].first][myUnstable[i].second]<<" "<<minD<< endl;
-
-		}
-		cout << "minD "<< minD<<endl;
-		return this->tryingFill(playerId, state, d, g, minD);
-	}
-	return 0;
-}
-bool Helper::findPath(const pair<int, int>& pos, 
-					vector<string> &mark, 
-					int playerId, 
-					State& state,
-					const vector<vector<int> >& d, 
-					const vector<vector<int> >& g, 
-					const int& minD)
-{
-	// cout << "visit "<<pos.first<<" "<<pos.second<<endl;
-	mark[pos.first][pos.second] = '1';
-	if( state.table[pos.first][pos.second] - '0' == playerId * 2 - 1) return true;
-	bool ret = false;
-	for(int j = 0; j < 4; j++) 
-	{
-		pair<int, int> newPos = make_pair(pos.first + dir_x[j], pos.second + dir_y[j]);
-		// cout <<"TO "<< newPos.first <<" "<<newPos.second<<endl;
-		// cout <<"Sum " << d[newPos.first][newPos.second] + g[newPos.first][newPos.second] << endl;
-		// cout <<"    " << g[newPos.first][newPos.second] <<" "<< g[pos.first][pos.second] << endl;
-		if( !helper.isOuter(newPos) && 	
-			mark[newPos.first][newPos.second] == '0' &&
-			d[newPos.first][newPos.second] + g[newPos.first][newPos.second] < 2*minD &&
-			(state.playersPos[playerId-1] == pos ||
-			g[newPos.first][newPos.second] == g[pos.first][pos.second] - 1) &&
-			state.table[newPos.first][newPos.second] - '0' != playerId * 2
-			)
-			ret |= this->findPath(newPos, mark, playerId, state, d, g, minD);
-	}
-	if( ret )
-		state.table[pos.first][pos.second] = playerId * 2 + '0';
-	return ret;
-}
-int Helper::tryingFill(const int& playerId, State& state,const vector<vector<int> >& d, const vector<vector<int> >& g, const int& minD)
-{
-	vector<string> mark = vector<string>(20, EMPTY_LINE);
-	bool isAlive = this->findPath(state.playersPos[playerId-1], mark, playerId, state, d, g, minD);
-	// cout << "-----------------" << endl;
-	// for(int i = 0; i < 20; i++) cout << state.table[i] << endl;
-	if( isAlive)
-		return helper.stablize(playerId, state);
-	return -oo;
+	this->lastStep = this->steps.front(); this->steps.pop();
+	return this->lastStep;
 }
 class Splix
 {
@@ -529,26 +340,30 @@ public:
 		for(int i = 1; i <= this->numberOfPlayer; i++)
 		{
 			cout << "Score of Player " << i <<" is " << this->players[i-1].getScore(i) <<endl;
+			cout << "Path " << endl;
+			queue<string> _steps = this->players[i-1].steps;
+			while(!_steps.empty())
+			{
+				cout <<_steps.front() <<" ";
+				_steps.pop();
+			}
+			cout << endl;
 		}
 	}
 };
 int main()
 {
+	freopen("output.txt", "w", stdout);
 	srand (time(NULL));
 	Splix splix = Splix(2);
 	// splix.testConstructor();
-	for(int step = 0; step < 100; step++) {
+	for(int step = 0; step < 50; step++) {
 		splix.updateStatePlayer();
 		splix.printPulse();
 		splix.nextTurn();
 	}
 	splix.updateStatePlayer();
 	splix.printPulse();
-	for(int i = 1; i <= splix.numberOfPlayer; i++)
-	{
-		cout << "evaluate Score of Player " << i << " is ";
-		cout << helper.evaluateScore(i, splix.states[splix.states.size()-1]) << endl;
-	}
 	return 0;
 }
 
@@ -634,7 +449,8 @@ void Splix::nextTurn()
 		decision = this->players[i].getDecision();
 		time (&end);
 		double dif = difftime (end,start);
-		cout <<" Player "<< i +1 <<" decide in "<< dif<<" seconds."<<endl;
+		// cout <<" Player "<< i +1 <<" decide in "<< dif<<" seconds."<<endl;
+		printf("Player %d decide in %.2lf\n seconds.", i+1, dif);
 		cout << "Player "<< i+1<<" decide go: "<<decision<<endl;
 		lastState = helper.updateGame(i+1, this->states, lastState, decision);
 	}
@@ -740,6 +556,7 @@ int Helper::stablize(int playerId, State &state)
 			}
 		}
 	}
+	// cout <<"[STABLiZE] "<<ret << endl;
 	return ret;
 }
 int Helper::currentDirect(int playerId, const vector<State>& states)
